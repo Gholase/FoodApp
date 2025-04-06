74187896d5c893a58c00e8a9fad894f236c7e0f9
--replace-all <priya0560patil@gmail.com>
2025-04-06 12:26:21 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "../styles/header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-content">
+        <img />
+        <h1>Food App</h1>
+      </div>
+      <nav>
+        <button>Cart</button>
+      </nav>
+    </header>
+  );
+}
